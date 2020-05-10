f533de686ec3d6214de2818f9f069ea6a3d5d8d6
sarrufat <arrufat.sergi@gmail.com>
2020-05-10 17:16:59 +0200

    Running Eureka Server

--- PhotoAppDiscoveryService/src/main/java/com/example/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/example/PhotoAppDiscoveryService/PhotoAppDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.PhotoAppDiscoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PhotoAppDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
